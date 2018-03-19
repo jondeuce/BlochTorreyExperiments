c07dbec4877cb38791395df47e35935a3f973986
Jonathan Doucette <jdoucette@phas.ubc.ca>
2018-03-19 14:04:29 -0700

    ps scripts for filtering/plotting result iterations

---
+++ Experiments/PerfusionOrientation/ParameterFitting/ParticleSwarm/PSPruneIterations.m
@@ -0,0 +1,43 @@
+%% load in data
+files = dir('*.mat');
+endsWith = @(str,ending) strcmpi( str(max(end-numel(ending)+1,1):end), ending );
+if endsWith(files(end).name, 'ParticleSwarmFitResults.mat')
+    res_file = files(end);
+    files = files(1:end-1);
+else
+    res_file = [];
+end
+
+r = load(files(1).name);
+Results = repmat(r.Results, numel(files), 1);
+for ii = 2:length(files)
+    r = load(files(ii).name);
+    Results(ii) = r.Results;
+end
+if ~isempty(res_file)
+    LsqResults = load(res_file.name);
+else
+    LsqResults = [];
+end
+
+%% plot results by error value
+dR2 = cat(1, Results.dR2);
+dR2_Data = Results(1).dR2_Data;
+Weights = Results(1).args.Weights;
+
+% calc error and sort
+lsqerr = sqrt( sum( bsxfun(@times, Weights, bsxfun(@minus, dR2, dR2_Data).^2), 2 ) );
+[sorted_err, sorted_inds] = sort(lsqerr);
+
+%% Keep best NumToKeep iterations
+NumToKeep = min(20, numel(Results));
+for ii = vec(sorted_inds(NumToKeep+1:end)).'
+    [pathstr,fname,ext] = fileparts(files(ii).name);
+    fprintf('%s\n',fname)
+    delete([fname,'.mat']);
+    delete([fname,'.png']);
+end
+
+%% delete diary
+diaryfile = dir('*.txt');
+delete(diaryfile.name)
