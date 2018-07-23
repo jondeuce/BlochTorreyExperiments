b176925d4bbabd64b81301aee1d5b3380571ebc8
jondeuce <jdoucette@phas.ubc.ca>
2018-07-22 20:19:29 -0700

    normest1 using AbstractMatrix

--- Experiments/MyelinWaterOrientation/Utils/normest1_slow.jl
+++ Experiments/MyelinWaterOrientation/Utils/normest1_slow.jl
@@ -6,9 +6,17 @@ module Normest1
 
 export normest1 #, test_afun, mass_and_stifness_afun
 
-function normest1(A::AbstractMatrix{Te};
+abstract type Normest1Flag end
+struct Dim <: Normest1Flag end
+struct Real <: Normest1Flag end
+struct Notransp <: Normest1Flag end
+struct Transp <: Normest1Flag end
+
+function normest1(A;
                   t::Int = 2,
-                  X::AbstractMatrix{Te} = Array{Te}[]) where Te
+                  X = nothing,
+                  Tf::Type{<:AbstractFloat} = Float64,
+                  varargin = ())
 #NORMEST1 Estimate of 1-norm of matrix by block 1-norm power method.
 #   C = NORMEST1(A) returns an estimate C of norm(A,1), where A is N-by-N.
 #   A can be an explicit matrix or a function AFUN such that
@@ -17,7 +25,7 @@ function normest1(A::AbstractMatrix{Te};
 #     "dim"      N
 #     "real"     1 if A is real, 0 otherwise
 #     "notransp" A*X
-#     "transp"   A'*X
+#     "transp"   A"*X
 #
 #   C = NORMEST1(A,T) changes the number of columns in the iteration matrix
 #   from the default 2.  Choosing T <= N/4 is recommended, otherwise it should
@@ -58,9 +66,18 @@ function normest1(A::AbstractMatrix{Te};
 #   Nicholas J. Higham
 #   Copyright 1984-2012 The MathWorks, Inc.
 
-   @assert size(A,1) == size(A,2) "AbstractMatrix A must be square"
-   n = size(A,2)
-   A_is_real = isreal(A)
+   # Determine whether A is a matrix or a function.
+   # if isa(A, AbstractMatrix{<:AbstractFloat}) #isfloat(A) && ismatrix(A)
+   #    n = maximum(size(A))
+   #    A_is_real = isreal(A)
+   # elseif isa(A,Function)
+   #    n = normapp(A,Dim(),nothing,varargin)
+   #    A_is_real = normapp(A,Real(),nothing,varargin)
+   # else
+   #    #error(message("MATLAB:normest1:ANotMatrixOrFunction"))
+   # end
+   n, A_is_real = A_properties(A, varargin)
+   Tc = A_is_real ? Tf : Complex{Tf}
 
    prnt = (t < 0)
    t = abs(t)
@@ -71,10 +88,14 @@ function normest1(A::AbstractMatrix{Te};
    rpt_S = 0; rpt_e = 0
 
    if t == n || n <= 4
-      # Get full matrix
-      Y = zeros(Te, n, n)
-      X = eye(Te, n)
-      A_mul_B!(Y, A, X)
+      # if isa(A, AbstractMatrix{<:AbstractFloat}) #isfloat(A)
+      #    Y = A
+      # else
+      #    X = eye(n)
+      #    # Y = normapp(A,"notransp",X,varargin)
+      #    Y = normapp(A,Notransp(),X,varargin)
+      # end
+      Y = A_full(A, varargin)
 
       # equivalent to `temp, m = sort( sum(abs(Y)) )` in MATLAB
       temp = sum(abs, Y, 1)
@@ -82,8 +103,8 @@ function normest1(A::AbstractMatrix{Te};
       temp = temp[:,m]
 
       est = temp[n] # `full(temp[n])` is unnecessary; elements of sparse matrices are not sparse in Julia
-      v = zeros(Te, n)
-      v[m[n]] = one(Te)
+      v = zeros(Tc,n,1)
+      v[m[n]] = one(Tc)
       w = Y[:,m[n]]
 
       iter = [0 1]
@@ -93,11 +114,11 @@ function normest1(A::AbstractMatrix{Te};
       return est, v, w, iter
    end
 
-   if isempty(X)
-      X = ones(Te, n, t)
+   if X == nothing
+      X = ones(Tc,n,t)
       #X[:,2:t] = mysign.(2*rand(n,t-1) - ones(n,t-1))
-      @views X[:,2:t] = rand(Te.(-1:2:1), n, t-1)
-      X, r = undupli(X, Array{Te}[], prnt)
+      @views X[:,2:t] = rand(Tc.(-1:2:1), n, t-1)
+      X, r = undupli(X, nothing, prnt)
       X ./= n
    end
 
@@ -109,12 +130,12 @@ function normest1(A::AbstractMatrix{Te};
    it = 0
    nmv = 0
 
-   ind = zeros(Int, t)
+   ind = zeros(Int,t,1)
    est_old = 0
    est_j = 0 #TODO make sure this is a good initialization
 
-   # S_type = A_is_real ? Int : Te
-   S = zeros(Te, n, t)
+   # S_type = A_is_real ? Int : Tc
+   S = zeros(Tc,n,t)
    S_old = similar(S)
    Y = similar(X)
    Z = similar(X)
@@ -122,7 +143,13 @@ function normest1(A::AbstractMatrix{Te};
    while true
        it += 1
 
-       A_mul_B!(Y, A, X)
+       # if isa(A, AbstractMatrix{<:AbstractFloat}) # isfloat(A)
+       #    A_mul_B!(Y, A, X)
+       # else
+       #    # Y = normapp(A,"notransp",X,varargin)
+       #    Y = normapp(A,Notransp(),X,varargin)
+       # end
+       A_apply!(Y, A, Notransp(), X, varargin)
        nmv += 1
 
        vals = sum(abs, Y, 1)
@@ -171,7 +198,13 @@ function normest1(A::AbstractMatrix{Te};
           rpt_S = rpt_S + r
        end
 
-       Ac_mul_B!(Z, A, X)
+       # if isa(A, AbstractMatrix{<:AbstractFloat}) # isfloat(A)
+       #    Ac_mul_B!(Z, A, S)
+       # else
+       #    # Z = normapp(A,"transp",S,varargin)
+       #    Z = normapp(A,Transp(),S,varargin)
+       # end
+       A_apply!(Z, A, Transp(), X, varargin)
        nmv = nmv + 1
 
        # Faster version of `for i=1:n, Zvals[i] = norm(Z[i,:], inf); end`:
@@ -224,9 +257,9 @@ function normest1(A::AbstractMatrix{Te};
           ind_hist = [ind_hist; ind[1:imax]]
        end
 
-       X = zeros(Te, n, t)
+       X = zeros(Tc,n,t)
        for j=1:imax
-          X[ind[j],j] = one(Te)
+          X[ind[j],j] = 1
        end
    end
 
@@ -244,11 +277,9 @@ function normest1(A::AbstractMatrix{Te};
       end
    end
 
-   iter = [it; nmv]
-   red = [rpt_S; rpt_e]
+   iter = [it; nmv]; red = [rpt_S; rpt_e]
 
-   v = zeros(Te, n)
-   v[est_j] = one(Te)
+   v = zeros(Tc,n,1); v[est_j] = 1
    if !prnt
       return est, v, w, iter
    end
@@ -267,38 +298,53 @@ end
 mysign(x::T) where {T<:AbstractFloat} = ifelse(x < zero(T), -one(T), one(T))
 mysign(x::Complex{T}) where {T<:AbstractFloat} = ifelse(x == zero(x), one(x), x/abs(x))
 
+#A_PROPERTIES Get dimension and check for realness
+A_properties(A, varargin) = (maximum(size(A)), isreal(A)) # fallback
+A_properties(A::Function, varargin) = (normapp(A,Dim(),nothing,varargin), normapp(A,Real(),nothing,varargin))
+
+#A_FULL Get full matrix A (small matrix optimization)
+A_full(A, varargin) = A # fallback
+A_full(A::Function, varargin) = (n = normapp(A,Dim(),nothing,varargin); return normapp(A,Notransp(),eye(n),varargin))
+
+#A_APPLY Call the function A on the input X
+A_apply!(Y, A, flag::Notransp, X, varargin) = A_mul_B!(Y, A, X) # fallback
+A_apply!(Y, A, flag::Transp, X, varargin) = Ac_mul_B!(Y, A, X) # fallback
+A_apply!(Y, A::Function, flag::Notransp, X, varargin) = copy!(Y, normapp(A,flag,X,varargin))
+A_apply!(Y, A::Function, flag::Transp, X, varargin) = copy!(Y, normapp(A,flag,X,varargin))
+
 #UNDUPLI   Look for and replace columns of S parallel to other columns of S or
 #          to columns of Sold.
-function undupli(S::AbstractMatrix{Te},
-                 S_old::AbstractMatrix{Te},
-                 prnt::Bool) where Te
+function undupli(S::AbstractArray,
+                 S_old::Union{Void,AbstractArray},
+                 prnt::Bool)
+   T = eltype(S)
    n, t = size(S)
    r = 0
    if t == 1
       return S, r
    end
 
-   if isempty(S_old) # Looking just at S.
+   if S_old == nothing # Looking just at S.
       # W = [S[:,1] zeros(n,t-1)]
-      W = zeros(Te,n,t)
+      W = zeros(T,n,t)
       @views W[:,1] = S[:,1]
       jstart = 2
       last_col = 1
    else              # Looking at S and S_old.
       # W = [S_old zeros(n,t-1)]
       ncols = size(S_old,2)
-      W = zeros(Te,n,t-1+ncols)
+      W = zeros(T,n,t-1+ncols)
       @views W[:,1:ncols] = S_old
       jstart = 1
       last_col = t
    end
 
-   sgn = zeros(Te,n)
+   sgn = zeros(T,n)
    for j=jstart:t
        rpt = 0
        while maximum(abs, @views S[:,j]'*W[:,1:last_col] ) == n
              rpt = rpt + 1
-             sgn .= rand(Te.(-1:2:1), n)
+             sgn .= rand(T.(-1:2:1), n)
              @views S[:,j] = sgn
              if rpt > n/t
                 break
@@ -318,18 +364,131 @@ function undupli(S::AbstractMatrix{Te},
 
 end
 
+#NORMAPP   Call matrix operator and error gracefully.
+#   NORMAPP(AFUN,FLAG,X) calls matrix operator AFUN with flag
+#   FLAG and matrix X.
+#   NORMAPP(AFUN,FLAG,X,...) allows extra arguments to
+#   AFUN(FLAG,X,...).
+#   NORMAPP is designed for use by NORMEST1.
+function normapp(afun,flag::Normest1Flag,x,varargin=())
+
+   y = try
+      afun(flag,x,varargin...)
+   catch ME
+      #error(message("MATLAB:normest1:Failure", string( afun ), ME.message))
+      rethrow(ME)
+   end
+   check_normapp(flag,x,y)
+
+   return y
+end
+
+function check_normapp(::Union{Notransp,Transp}, x, y)
+   if !isequal(size(y),size(x))
+      #error(message("MATLAB:normest1:MatrixSizeMismatchFlag", string( afun ), size( x, 1 ), size( x, 2 ), flag))
+      error("Output size must equal input size.")
+   end
+   return true
+end
+
+function check_normapp(::Dim, x, y)
+   if !isa(y, Int) || y < 0
+      #error(message("MATLAB:normest1:NegInt", string( afun ), flag))
+      error("Dim must be a positive integer.")
+   end
+   return true
+end
+
+function check_normapp(::Real, x, y)
+   if !isa(y, Bool)
+      #error(message("MATLAB:normest1:NegInt", string( afun ), flag))
+      error("Real must return a boolean.")
+   end
+   return true
+end
+
+# function normapp(afun,flag,x,varargin=())
+# #NORMAPP   Call matrix operator and error gracefully.
+# #   NORMAPP(AFUN,FLAG,X) calls matrix operator AFUN with flag
+# #   FLAG and matrix X.
+# #   NORMAPP(AFUN,FLAG,X,...) allows extra arguments to
+# #   AFUN(FLAG,X,...).
+# #   NORMAPP is designed for use by NORMEST1.
+#
+#    y = try
+#       afun(flag,x,varargin...)
+#    catch ME
+#       #error(message("MATLAB:normest1:Failure", string( afun ), ME.message))
+#       rethrow(ME)
+#    end
+#
+#    if isequal(flag,"notransp") || isequal(flag,"transp")
+#       if !isequal(size(y),size(x))
+#          #error(message("MATLAB:normest1:MatrixSizeMismatchFlag", string( afun ), size( x, 1 ), size( x, 2 ), flag))
+#       end
+#    end
+#
+#    if isequal(flag,"dim")
+#       if y != round(y) || y < 0
+#          #error(message("MATLAB:normest1:NegInt", string( afun ), flag))
+#       end
+#    end
+#
+#    if isequal(flag,"real")
+#       if y != 0 && y != 1
+#          #error(message("MATLAB:normest1:Not0or1", string( afun ), flag))
+#       end
+#    end
+#
+#    return y
+#
+# end
+#
 # # test `afun` where a matrix `A` is wrapped and given as a varargin parameter
-# test_afun(flag::Notransp,x,A) = A*x
-# test_afun(flag::Transp,x,A) = A'*x
-# test_afun(flag::Dim,x,A) = size(A,1)
-# test_afun(flag::Real,x,A) = isreal(A)
+# function test_afun(flag,x,A)
+#    if flag == "notransp"
+#       return A*x
+#    elseif flag == "transp"
+#       return A'*x
+#    elseif flag == "dim"
+#       return size(A,1)
+#    elseif flag == "real"
+#       return isreal(A)
+#    else
+#       error("Unknown flag: $flag")
+#    end
+# end
 #
 # # test `afun` where stiffness matrix `K` and factored mass matrix `Mfact` are
 # # wrapped and given as a varargin parameters
-# mass_and_stifness_afun(flag::Notransp,x,K,Mfact) = (tmp = similar(x); A_mul_B!(tmp, K, x); return Mfact\tmp)
-# mass_and_stifness_afun(flag::Transp,x,K,Mfact) = (tmp = Mfact\x; return Ac_mul_B(K, tmp)) # (inv(M)*K)'*x == K'*inv(M)*x (M is symmetric)
-# mass_and_stifness_afun(flag::Dim,x,K,Mfact) = size(K,1)
-# mass_and_stifness_afun(flag::Real,x,K,Mfact) = isreal(K) && isreal(M)
+# function mass_and_stifness_afun(flag,x,K,Mfact)
+#    if flag == "notransp" # inv(M)*K*x
+#       tmp = similar(x)
+#       A_mul_B!(tmp, K, x)
+#       return Mfact\tmp
+#    elseif flag == "transp" # (inv(M)*K)'*x == K'*inv(M)*x (M is symmetric)
+#       tmp = Mfact\x
+#       return Ac_mul_B(K, tmp)
+#    elseif flag == "dim"
+#       return size(K,1)
+#    elseif flag == "real"
+#       return isreal(K) && isreal(M)
+#    else
+#       error("Unknown flag: $flag")
+#    end
+# end
+# test `afun` where a matrix `A` is wrapped and given as a varargin parameter
+test_afun(flag::Notransp,x,A) = A*x
+test_afun(flag::Transp,x,A) = A'*x
+test_afun(flag::Dim,x,A) = size(A,1)
+test_afun(flag::Real,x,A) = isreal(A)
+
+# test `afun` where stiffness matrix `K` and factored mass matrix `Mfact` are
+# wrapped and given as a varargin parameters
+mass_and_stifness_afun(flag::Notransp,x,K,Mfact) = (tmp = similar(x); A_mul_B!(tmp, K, x); return Mfact\tmp)
+mass_and_stifness_afun(flag::Transp,x,K,Mfact) = (tmp = Mfact\x; return Ac_mul_B(K, tmp)) # (inv(M)*K)'*x == K'*inv(M)*x (M is symmetric)
+mass_and_stifness_afun(flag::Dim,x,K,Mfact) = size(K,1)
+mass_and_stifness_afun(flag::Real,x,K,Mfact) = isreal(K) && isreal(M)
 
 using Base.Test
 using ProfileView
@@ -347,8 +506,8 @@ function test()
       A, Ac = RealAndCplx(n)
       @test normest1(A)[1] ≈ norm(A,1)
       @test normest1(Ac)[1] ≈ norm(Ac,1)
-      # @test normest1(test_afun; varargin=(A,))[1] ≈ norm(A,1)
-      # @test normest1(test_afun; varargin=(Ac,))[1] ≈ norm(Ac,1)
+      @test normest1(test_afun; varargin=(A,))[1] ≈ norm(A,1)
+      @test normest1(test_afun; varargin=(Ac,))[1] ≈ norm(Ac,1)
    end
 
    # Larger matrices: should be exact when t = size(A,1)
@@ -356,8 +515,8 @@ function test()
       A, Ac = RealAndCplx(n)
       @test normest1(A; t=n)[1] ≈ norm(A,1)
       @test normest1(Ac; t=n)[1] ≈ norm(Ac,1)
-      # @test normest1(test_afun; t=n, varargin=(A,))[1] ≈ norm(A,1)
-      # @test normest1(test_afun; t=n, varargin=(Ac,))[1] ≈ norm(Ac,1)
+      @test normest1(test_afun; t=n, varargin=(A,))[1] ≈ norm(A,1)
+      @test normest1(test_afun; t=n, varargin=(Ac,))[1] ≈ norm(Ac,1)
    end
 
    # Larger matrices: should almost always be exact when t = size(A,1)/2
@@ -365,8 +524,8 @@ function test()
       A, Ac = RealAndCplx(n)
       @test normest1(A; t=div(n,2))[1] ≈ norm(A,1)
       @test normest1(Ac; t=div(n,2))[1] ≈ norm(Ac,1)
-      # @test normest1(test_afun; t=div(n,2), varargin=(A,))[1] ≈ norm(A,1)
-      # @test normest1(test_afun; t=div(n,2), varargin=(Ac,))[1] ≈ norm(Ac,1)
+      @test normest1(test_afun; t=div(n,2), varargin=(A,))[1] ≈ norm(A,1)
+      @test normest1(test_afun; t=div(n,2), varargin=(Ac,))[1] ≈ norm(Ac,1)
    end
 
 end
