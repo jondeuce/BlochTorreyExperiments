ddaae66359e7a0dc0f321d9843af977332c2feab
Jon <jon-doucette@hotmail.com>
2018-12-13 08:44:53 -0800

    finite difference neumann simulation testing working

---
+++ Experiments/MyelinWaterOrientation/Test/testblochtorrey2D.jl
@@ -0,0 +1,148 @@
+#TESTBLOCHTORREY2D testblochtorrey2D(;kwargs...)
+#
+# Example usage:
+#   U = testblochtorrey2D(D_Axon = 1.0, D_Tissue = 1.0, D_Sheath = 1.0, Plot = true)
+function testblochtorrey2D(;
+        Npts::Int           = 101,          # Number of points per dimension
+        Domain::Vector{T}   = [-1.0, 1.0],  # Bounds for side of square domain
+        R_outer_rel::T      = 1.0,          # Outer radius relative to R_mu
+        Time::T             = 60e-3,        # Simulation time
+        Plot::Bool          = true,         # Plot resulting magnitude and phase
+        kwargs...
+    ) where {T}
+
+    opts = BlochTorreyParameters{T}(;kwargs...)
+    @assert opts.D_Axon == opts.D_Tissue == opts.D_Sheath
+
+    D = opts.D_Tissue
+    a, b = Domain
+    h = (b-a)/(Npts-1)
+    pts = range(a, stop = b, length = Npts)
+
+    Gamma(x,y) = complex(r2decay(x,y,R_outer_rel,opts), omega(x,y,R_outer_rel,opts))
+    G = Complex{T}[Gamma(x,y) for x in pts, y in pts]
+
+    g  = opts.g_ratio # 0.8
+    ro = R_outer_rel * opts.R_mu #0.5
+    ri = g*ro
+    m_int = Bool[x^2 + y^2 < ri^2 for x in pts, y in pts]
+    m_ext = Bool[x^2 + y^2 > ro^2 for x in pts, y in pts]
+
+    tmp = similar(G)
+    function A(du,u,isadj::Bool=false)
+        # du .= D .* lap(u, h, h, m_int, m_ext) .- G .* u
+        u = reshape(u, Npts, Npts)
+        du = reshape(du, Npts, Npts)
+        lap!(du, u, h, h, m_int, m_ext)
+        du .*= D
+        isadj ? (tmp .= conj.(G) .* u) : (tmp .= G .* u)
+        axpy!(-one(real(T)), tmp, du)
+        return du
+    end
+    Amap = FunctionMap{Complex{T}}(
+        (du,u) -> A(du,u,false), (du,u) -> A(du,u,true), Npts^2;
+        issymmetric = true, ishermitian = false, ismutating = true)
+
+    U0 = fill(one(T)*im, Npts^2)
+    U = similar(U0)
+    @time U = Expokit.expmv!(U, Time, Amap, U0)
+
+    U0 = reshape(U0, Npts, Npts)
+    U = reshape(U, Npts, Npts)
+
+    # unwrap = @(x) sliceND(unwrapLap(repmat(x,1,1,11)),6,3)
+    # figure, imagesc(rot90(unwrap(angle(U)))); axis image; title phase; colorbar
+    # figure, imagesc(rot90(abs(U))); axis image; title magnitude; colorbar
+
+    return U, Amap, U0, G
+end
+
+function omega(
+        x::T,
+        y::T,
+        R_outer_rel::T = 1.0, # Outer radius relative to R_mu
+        opts::BlochTorreyParameters{T} = BlochTorreyParameters{T}()
+    ) where {T}
+
+    B0    = opts.B0    # -3.0         # External magnetic field (z-direction) [T]
+    gamma = opts.gamma # 2.67515255e8 # Gyromagnetic ratio [rad/s/T]
+    w0    = gamma * B0 # Resonance frequency [rad/s]
+    th    = opts.theta # pi/2   # Main magnetic field angle w.r.t B0 [rad]
+    c2    = cos(th)^2
+    s2    = sin(th)^2
+    ChiI  = opts.ChiI # -60e-9  # Isotropic susceptibility of myelin [ppb] (check how to get it) (Xu et al. 2017)
+    ChiA  = opts.ChiA # -120e-9 # Anisotropic Susceptibility of myelin [ppb] (Xu et al. 2017)
+    E     = opts.E    #  10e-9  # Exchange component to resonance freqeuency [ppb] (Wharton and Bowtell 2012)
+
+    g  = opts.g_ratio # 0.8
+    ro = R_outer_rel * opts.R_mu #0.5
+    ri = g*ro
+    ri2 = ri^2
+    ro2 = ro^2
+
+    r2 = x^2 + y^2
+    r = sqrt(r2)
+    t = atan(y,x)
+
+    w = if (r < ri)
+        w0 * ChiA * 3*s2/4 * log(ro/ri)
+    elseif (ri <= r && r <= ro)
+        (w0 * ChiI/2) * (c2 - 1/3 - s2 * cos(2*t) * (ri2/r2)) +
+        (w0 * E) +
+        (w0 * ChiA) * (s2 * (-5/12 - cos(2*t)/8 * (1+ri2/r2) + (3/4) * log(ro/r)) - c2/6)
+    else
+        (w0 * ChiI * (s2/2)) * cos(2*t) * (ro2 - ri2) / r2 +
+        (w0 * ChiA * (s2/8)) * cos(2*t) * (ro2 - ri2) / r2
+    end
+
+    return w
+end
+
+function r2decay(
+        x::T,
+        y::T,
+        R_outer_rel::T = 1.0, # Outer radius relative to R_mu
+        opts::BlochTorreyParameters{T} = BlochTorreyParameters{T}()
+    ) where {T}
+
+    R2_sp = opts.R2_sp # 1/15e-3 # Relaxation rate of small pool [s^-1] (Myelin) (Xu et al. 2017) (15e-3s)
+    R2_lp = opts.R2_lp # 1/63e-3 # Relaxation rate of large pool [s^-1] (Intra/Extra-cellular)
+
+    g  = opts.g_ratio # 0.8
+    ro = R_outer_rel * opts.R_mu #0.5
+    ri = g*ro
+    ri2 = ri^2
+    ro2 = ro^2
+    r2 = x^2 + y^2
+
+    R2 = ri2 <= r2 && r2 <= ro2 ? R2_sp : R2_lp
+
+    return R2
+end
+
+# struct BTOp{T,Tr,N}
+#     G::AbstractArray{T,N}
+#     h::NTuple{N,Tr}
+#     m_int::AbstractArray{Bool,N}
+#     m_ext::AbstractArray{Bool,N}
+# end
+# function LinearAlgebra.mul!(du::AbstractArray{T,2}, A::BTOp{T}, u::AbstractArray{T,2}) where {T}
+#     # return lap(u, A.h[1], A.h[2], A.m_int, A.m_ext) .- A.G .* u
+#     lap!(du, u, A.h[1], A.h[2], A.m_int, A.m_ext)
+#     axpy!(-one(real(T)), A.G .* u, du)
+#     return du
+# end
+# Base.size(A::BTOp) = (length(A.G), length(A.G))
+# Base.size(A::BTOp, d::Int) = (d == 1 || d == 2) ? length(A.G) : 1
+# Base.eltype(A::BTOp{T}) where {T} = T
+# Base.:*(a::Number, A::BTOp) = BTOp(a.*A.G, A.h./sqrt(a), A.m_int, A.m_ext)
+# LinearAlgebra.opnorm(A::BTOp{T}, p) where {T} = one(T)
+
+# function testblochtorrey2D(;
+#         kwargs...
+#     )
+#
+#     @show kwargs
+#
+#     return nothing
+# end
--- Experiments/MyelinWaterOrientation/Test/testneumannfindiff.jl
+++ Experiments/MyelinWaterOrientation/Test/testneumannfindiff.jl
@@ -1,21 +1,50 @@
 function lap!(
         l::AbstractArray{T,2},
         x::AbstractArray{T,2},
-        m_ext::AbstractArray{Bool,2},
-        m_int::AbstractArray{Bool,2},
-        hx::T = one(T),
-        hy::T = one(T)
+        hx = one(real(T)),
+        hy = one(real(T)),
+        m_int::AbstractArray{Bool,2} = falses(size(x)),
+        m_ext::AbstractArray{Bool,2} = falses(size(x))
     ) where {T}
 
-    @inline is_ext(i,j) = m_ext[i,j]
-    @inline is_int(i,j) = m_ext[i,j]
-    @inline is_mye(i,j) = !(m_int[i,j] || m_ext[i,j])
+    @inline is_ext(i,j) = @inbounds m_ext[i,j]
+    @inline is_int(i,j) = @inbounds m_int[i,j]
+    @inline is_mye(i,j) = @inbounds !(m_int[i,j] || m_ext[i,j])
 
-    for j in 1:size(x,2)
-        for i in 1:size(x,1)
-
-
+    Nx, Ny = size(x)
+    @inbounds for j in 1:Ny
+        for i in 1:Nx
+            l[i,j] = if is_ext(i,j)
+                dxB = (i == 1  || !is_ext(i-1,j  )) ? zero(T) : (x[i  ,j  ] - x[i-1,j  ])/hx
+                dxF = (i == Nx || !is_ext(i+1,j  )) ? zero(T) : (x[i+1,j  ] - x[i  ,j  ])/hx
+                dyB = (j == 1  || !is_ext(i  ,j-1)) ? zero(T) : (x[i  ,j  ] - x[i  ,j-1])/hy
+                dyF = (j == Ny || !is_ext(i  ,j+1)) ? zero(T) : (x[i  ,j+1] - x[i  ,j  ])/hy
+                (dxF - dxB)/hx + (dyF - dyB)/hy
+            elseif is_int(i,j)
+                dxB = (i == 1  || !is_int(i-1,j  )) ? zero(T) : (x[i  ,j  ] - x[i-1,j  ])/hx
+                dxF = (i == Nx || !is_int(i+1,j  )) ? zero(T) : (x[i+1,j  ] - x[i  ,j  ])/hx
+                dyB = (j == 1  || !is_int(i  ,j-1)) ? zero(T) : (x[i  ,j  ] - x[i  ,j-1])/hy
+                dyF = (j == Ny || !is_int(i  ,j+1)) ? zero(T) : (x[i  ,j+1] - x[i  ,j  ])/hy
+                (dxF - dxB)/hx + (dyF - dyB)/hy
+            else # is_mye(i,j)
+                dxB = (i == 1  || !is_mye(i-1,j  )) ? zero(T) : (x[i  ,j  ] - x[i-1,j  ])/hx
+                dxF = (i == Nx || !is_mye(i+1,j  )) ? zero(T) : (x[i+1,j  ] - x[i  ,j  ])/hx
+                dyB = (j == 1  || !is_mye(i  ,j-1)) ? zero(T) : (x[i  ,j  ] - x[i  ,j-1])/hy
+                dyF = (j == Ny || !is_mye(i  ,j+1)) ? zero(T) : (x[i  ,j+1] - x[i  ,j  ])/hy
+                (dxF - dxB)/hx + (dyF - dyB)/hy
+            end
         end
     end
 
+    return l
+end
+
+function lap(
+        x::AbstractArray{T,2},
+        hx = one(real(T)),
+        hy = one(real(T)),
+        m_int::AbstractArray{Bool,2} = falses(size(x)),
+        m_ext::AbstractArray{Bool,2} = falses(size(x))
+    ) where {T}
+    return lap!(zeros(T,size(x)),x,hx,hy,m_int,m_ext)
 end
