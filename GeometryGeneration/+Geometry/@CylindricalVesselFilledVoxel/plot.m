3181a5bb71d8897635401079741fcb2dd24470c9
Jonathan Doucette <jdoucette@phas.ubc.ca>
2019-01-24 16:52:10 -0800

    updating plot function for cylindericalvesselfilledvoxel

--- GeometryGeneration/+Geometry/@CylindricalVesselFilledVoxel/plot.m
+++ GeometryGeneration/+Geometry/@CylindricalVesselFilledVoxel/plot.m
@@ -1,27 +1,38 @@
-function [ h ] = plot( G, plotmajor, plotminor )
+function [ Fig ] = plot( G, plotmajor, plotminor, verb )
 %PLOT Plots the CylindricalVesselFilledVoxel G.
 
-if nargin < 2 || isempty(plotmajor); plotmajor = true; end
+if nargin < 4 || isempty(verb); verb = true; end
 if nargin < 3 || isempty(plotminor); plotminor = true; end
+if nargin < 2 || isempty(plotmajor); plotmajor = true; end
 
-H = [];
+% set(0,'DefaultFigureVisible','off');
+
 titlestr = '';
+fig = figure;
+hold on
 
 if plotminor
     col = 'b';
     alpha = 0.1;
-    H = plot_cylinders_in_box( G.p, G.vz, G.r, ...
-        G.VoxelSize, G.VoxelCenter, titlestr, H, col, alpha );
+    fig = plot_cylinders_in_box( G.p, G.vz, G.r, ...
+        G.VoxelSize, G.VoxelCenter, titlestr, fig, col, alpha, verb );
 end
 
 if plotmajor
     col = 'r';
     alpha = 0.5;
-    H = plot_cylinders_in_box( G.p0, G.vz0, G.r0, ...
-        G.VoxelSize, G.VoxelCenter, titlestr, H, col, alpha );
+    fig = plot_cylinders_in_box( G.p0, G.vz0, G.r0, ...
+        G.VoxelSize, G.VoxelCenter, titlestr, fig, col, alpha, false );
 end
 
-if nargout > 0; h = H; end
+% Set oblique viewing angle
+view([0.8, 0.5, 0.2]);
+
+% set(0,'DefaultFigureVisible','on');
+figure(fig);
+drawnow;
+
+if nargout > 0; Fig = fig; end
 
 end
 
