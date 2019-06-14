ca9c2a6e58275a3e0b88b85d1935085f0f673d5c
Jonathan Doucette <jdoucette@phas.ubc.ca>
2019-06-13 17:19:48 -0700

    running medium vessel sim, nmajor = 2

--- Experiments/PerfusionOrientation/ParameterFitting/FminCon/perforientation_fmincon.m
+++ Experiments/PerfusionOrientation/ParameterFitting/FminCon/perforientation_fmincon.m
@@ -72,7 +72,7 @@ Weights = Weights / sum(vec(Weights));
 
 % ======================== BLOCH-TORREY SETTINGS ======================== %
 
-Nmajor = 3;
+Nmajor = 2;
 Rminor_mu = 7.0;
 Rminor_sig = 0.0;
 % Rminor_mu = 13.7;
