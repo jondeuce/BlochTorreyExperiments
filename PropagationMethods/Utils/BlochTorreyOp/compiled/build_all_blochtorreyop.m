90a845bc5da92d18d5e1bc83d2bfe9505c0c87e7
Jon <jon-doucette@hotmail.com>
2019-06-29 11:07:27 -0700

    test immediately after building

--- PropagationMethods/Utils/BlochTorreyOp/compiled/build_all_blochtorreyop.m
+++ PropagationMethods/Utils/BlochTorreyOp/compiled/build_all_blochtorreyop.m
@@ -1,35 +1,10 @@
-try
-    build_BlochTorreyAction;
-catch e
-    warning(e.message);
-end
+% Build all compiled c/c++ files
+try, build_BlochTorreyAction, catch e, warning(e.message), end
+try, build_Laplacian, catch e, warning(e.message), end
+try, build_SevenPointDifferenceMasked, catch e, warning(e.message), end
+try, build_SevenPointStencil, catch e, warning(e.message), end
+try, build_SevenPointStencilMasked, catch e, warning(e.message), end
+try, build_SevenPointSumMasked, catch e, warning(e.message), end
 
-try
-    build_Laplacian;
-catch e
-    warning(e.message);
-end
-
-try
-    build_SevenPointDifferenceMasked;
-catch e
-    warning(e.message);
-end
-
-try
-    build_SevenPointStencil;
-catch e
-    warning(e.message);
-end
-
-try
-    build_SevenPointStencilMasked;
-catch e
-    warning(e.message);
-end
-
-try
-    build_SevenPointSumMasked;
-catch e
-    warning(e.message);
-end
+% Run BlochTorreyOp tests
+BlochTorreyOp.test;
