4c29e1b87921d3a075bdebd4a9d7439a495c237a
Jonathan Doucette <jdoucette@phas.ubc.ca>
2020-05-02 13:05:41 -0700

    running toy model cvae training on wrong model w/ true noise

--- MMDLearning/cvae.jl
+++ MMDLearning/cvae.jl
@@ -42,7 +42,7 @@ end
 const IS_TOY_MODEL = true #TODO
 const TEST_DATA_LEARNED_CORR = false #TODO learned correction and noise
 const TEST_DATA_LEARNED_NOISE = false #TODO learned noise only
-const TRAIN_DATA_LEARNED_CORR = true #TODO learned correction and noise
+const TRAIN_DATA_LEARNED_CORR = false #TODO learned correction and noise
 const TRAIN_DATA_LEARNED_NOISE = false #TODO learned noise only
 const mmd_settings = load_settings()
 const mmd_models = let
