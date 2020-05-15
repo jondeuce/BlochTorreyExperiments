795d7755aff503111bf058a9b5ff7e30f17c2a26
Jonathan Doucette <jdoucette@phas.ubc.ca>
2020-05-15 13:26:22 -0700

    plot gan results

--- MMDLearning/scripts/plot_sweep_results.jl
+++ MMDLearning/scripts/plot_sweep_results.jl
@@ -24,12 +24,27 @@ end
 flatten_dict(din::Dict{<:AbstractString, Any}) = flatten_dict!(Dict{String, Any}(), din)
 
 # DataFrame has a constructor from Dict types already, but it broadcasts over vector values
-dict_to_df(d::Dict) = push!(DataFrame(typeof.(values(d)), Symbol.(keys(d))), collect(values(d)))
+dict_to_df(d::Dict)::DataFrame = push!(DataFrame(typeof.(values(d)), Symbol.(keys(d))), collect(values(d)))
 
-function read_to_dataframe(file, field)
-    return endswith(file, ".toml") ? dict_to_df(flatten_dict(TOML.parsefile(file)[field])) :
-           endswith(file, ".bson") ? DataFrame(BSON.load(file)[field]) :
-           error("Unsupported file type")
+function read_to_dataframe(file, field; max_retry::Int = 10)
+    if endswith(file, ".toml")
+        return dict_to_df(flatten_dict(TOML.parsefile(file)[field]))
+    elseif endswith(file, ".bson")
+        for i in 0:max_retry
+            try
+                return DataFrame(BSON.load(file)[field])
+            catch e
+                if e isa EOFError
+                    @warn "Read end of file: $file\nRetry $i/$max_retry..."
+                    sleep(5.0)
+                else
+                    rethrow(e)
+                end
+            end
+        end
+    else
+        error("Unsupported file type")
+    end
 end
 
 function dataframe_template(results_dir, file, field)
@@ -52,6 +67,10 @@ function read_results(results_dir)
         if "sweep_settings.toml" ∈ files && "settings.toml" ∈ files && "current-progress.bson" ∈ files #TODO: && "best-progress.bson" ∈ files
             df_sweep = read_to_dataframe(joinpath(root, "sweep_settings.toml"), SWEEP_FIELD)
             df_curr = read_to_dataframe(joinpath(root, "current-progress.bson"), "progress")
+            if isnothing(df_curr)
+                @warn "Failed to load results: $root"
+                continue
+            end
 
             # ibest = findmin(df_curr.loss)[2]
             # ibest = findmin(df_curr.rmse)[2]
@@ -85,7 +104,8 @@ end
 # results_dir = "/project/st-arausch-1/jcd1994/MMD-Learning/toymmdopt_eps=1e-2/toymmdopt-v5"
 # results_dir = "/project/st-arausch-1/jcd1994/MMD-Learning/toymmdopt_eps=1e-2/toymmdopt-v7"
 # results_dir = "/project/st-arausch-1/jcd1994/MMD-Learning/mmdopt-v7"
-results_dir = "/project/st-arausch-1/jcd1994/simulations/MMD-Learning/ganopt-v1"
+# results_dir = "/project/st-arausch-1/jcd1994/simulations/MMD-Learning/ganopt-v1"
+results_dir = "/project/st-arausch-1/jcd1994/simulations/MMD-Learning/toyganopt-v1"
 sweep_dir = joinpath(results_dir, "sweep");
 df_best, df_curr, sweep_temp, prog_temp = read_results(sweep_dir);
 # df = df_best
@@ -109,6 +129,7 @@ end
 
 function make_plots(df, sweepcols, metric, thresh = 0.5)
     dfp = dropmissing(df, metric)
+    dfp = filter(r -> !any(x -> isa(x, Number) && isnan(x), r), dfp)
     if isempty(dfp)
         @info "metric $metric has all missing values"
         return nothing
