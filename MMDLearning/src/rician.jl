17b8f510cb906004a1d9d7ddf5fe2624d2e939d7
Jonathan Doucette <jdoucette@phas.ubc.ca>
2020-06-16 12:57:12 -0700

    replace series approx. of logbesseli etc. with besselix

--- MMDLearning/src/rician.jl
+++ MMDLearning/src/rician.jl
@@ -38,39 +38,51 @@ end
 @inline Base.eltype(::Type{Rician{T}}) where {T} = T
 
 #### Laguerre/Bessel functions
+#=
 @inline _L½_bessel_kernel(x::T) where {T} = exp(-x/2) * ((1 + x) * T(besseli(0, x/2)) + x * T(besseli(1, x/2)))
 @inline _L½_series_kernel(x::T) where {T} = (y = inv(x); @evalpoly(y, T(2), T(1)/2, T(1)/16, T(3)/64, T(75)/1024, T(735)/4096) * sqrt(x/pi))
-@inline laguerre½(x, t = 150) = -x < t ? _L½_bessel_kernel(float(-x)) : _L½_series_kernel(float(-x)) # note negative arguments for kernels
+@inline __laguerre½(x, t = 150) = -x < t ? _L½_bessel_kernel(float(-x)) : _L½_series_kernel(float(-x)) # note negative arguments for kernels
 
 @inline _logI0_bessel_kernel(x::T) where {T} = log(T(besseli(0, x)) + eps(T))
 @inline _logI0_series_kernel(x::T) where {T} = (y = inv(x); log(@evalpoly(y, T(1), T(1)/8, T(9)/128, T(75)/1024, T(3675)/32768, T(59535)/262144, T(2401245)/4194304, T(57972915)/33554432)) + x - log(x)/2 - log(2*T(π))/2)
-@inline logbesseli0(x, t = 75)  = x < t ? _logI0_bessel_kernel(float(x)) : _logI0_series_kernel(float(x))
+@inline _logbesseli0(x, t = 75)  = x < t ? _logI0_bessel_kernel(float(x)) : _logI0_series_kernel(float(x))
 
 @inline _logI1_bessel_kernel(x::T) where {T} = log(T(besseli(1, x)) + eps(T))
 @inline _logI1_series_kernel(x::T) where {T} = (y = inv(x); log(@evalpoly(y, T(1), -T(3)/8, -T(15)/128, -T(105)/1024, -T(4725)/32768, -T(72765)/262144, -T(2837835)/4194304, -T(66891825)/33554432, )) + x - log(x)/2 - log(2*T(π))/2)
-@inline logbesseli1(x, t = 75)  = x < t ? _logI1_bessel_kernel(float(x)) : _logI1_series_kernel(float(x))
+@inline _logbesseli1(x, t = 75)  = x < t ? _logI1_bessel_kernel(float(x)) : _logI1_series_kernel(float(x))
 
 @inline _I1I0m1_bessel_kernel(x::T) where {T} = T(besseli(1, x)) / T(besseli(0, x)) - 1
 @inline _I1I0m1_series_kernel(x::T) where {T} = (y = inv(x); y * @evalpoly(y, -T(1)/2, -T(1)/8, -T(1)/8, -T(25)/128, -T(13)/32, -T(1073)/1024, -T(103)/32, -T(375733)/32768, -T(23797)/512, -T(55384775)/262144))
-@inline besseli1i0m1(x, t = 50) = x < t ? _I1I0m1_bessel_kernel(float(x)) : _I1I0m1_series_kernel(float(x))
+@inline _besseli1i0m1(x, t = 50) = x < t ? _I1I0m1_bessel_kernel(float(x)) : _I1I0m1_series_kernel(float(x))
 
 @inline _I2I0_bessel_kernel(x::T) where {T} = T(besseli(2, x)) / T(besseli(0, x))
 @inline _I2I0_series_kernel(x::T) where {T} = (y = inv(x); @evalpoly(y, T(1), -T(2), T(1), T(1)/4, T(1)/4, T(25)/64, T(13)/16, T(1073)/512, T(103)/16, T(375733)/16384, T(23797)/256))
-@inline besseli2i0(x, t = 50)   = x < t ? _I2I0_bessel_kernel(float(x)) : _I2I0_series_kernel(float(x))
+@inline _besseli2i0(x, t = 50)   = x < t ? _I2I0_bessel_kernel(float(x)) : _I2I0_series_kernel(float(x))
+=#
+
+#### Laguerre/Bessel functions
+_besselix(ν::Real, x::Real) = SpecialFunctions.besselix(oftype(float(x), ν), float(x)) # besselix(ν, x) = Iν(x) * exp(-|x|)
+_logbesseli(ν::Real, x::Real) = log(_besselix(ν, x)) + abs(x) # since log(besselix(ν, x)) = log(Iν(x)) - |x| #TODO: log(⋅) -> log(⋅ + eps(typeof(float(x))))?
+_logbesseli0(x::Real) = _logbesseli(0, x)
+_logbesseli1(x::Real) = _logbesseli(1, x)
+_logbesseli2(x::Real) = _logbesseli(2, x)
+_besseli1i0m1(x::Real) = _besselix(1, x) / _besselix(0, x) - 1
+_besseli2i0(x::Real) = _besselix(2, x) / _besselix(0, x)
+_laguerre½(x::Real) = (x < 0 ? one(x) : exp(x)) * ((1 - x) * _besselix(0, -x/2) - x * _besselix(1, -x/2)) # besselix(ν, ±x/2) = Iν(±x/2) * exp(-|±x/2|) = Iν(-x/2) * exp(∓x/2)
 
 #### Statistics
-@inline Distributions.mean(d::Rician) = d.σ * sqrt(pi/2) * laguerre½(-d.ν^2 / 2d.σ^2)
+@inline Distributions.mean(d::Rician) = d.σ * sqrt(pi/2) * _laguerre½(-d.ν^2 / 2d.σ^2)
 # @inline Distributions.mode(d::Rician) = ?
 # @inline Distributions.median(d::Rician) = ?
 
-@inline Distributions.var(d::Rician) = 2 * d.σ^2 + d.ν^2 - pi * d.σ^2 * laguerre½(-d.ν^2 / 2d.σ^2)^2 / 2
+@inline Distributions.var(d::Rician) = 2 * d.σ^2 + d.ν^2 - pi * d.σ^2 * _laguerre½(-d.ν^2 / 2d.σ^2)^2 / 2
 @inline Distributions.std(d::Rician) = sqrt(var(d))
 # @inline Distributions.skewness(d::Rician{T}) where {T<:Real} = ?
 # @inline Distributions.kurtosis(d::Rician{T}) where {T<:Real} = ?
 # @inline Distributions.entropy(d::Rician) = ?
 
 #### Evaluation
-@inline Distributions.logpdf(d::Rician, x::Real) = log(x / d.σ^2 + eps(float(typeof(x)))) + logbesseli0(x * d.ν / d.σ^2) - (x^2 + d.ν^2) / (2*d.σ^2)
+@inline Distributions.logpdf(d::Rician, x::Real) = log(x / d.σ^2 + eps(float(typeof(x)))) + _logbesseli0(x * d.ν / d.σ^2) - (x^2 + d.ν^2) / (2*d.σ^2)
 @inline Distributions.pdf(d::Rician, x::Real) = exp(logpdf(d, x)) # below version errors for large x (besseli throws); otherwise is consistent
 # @inline Distributions.pdf(d::Rician, x::Real) = x * besseli(0, x * d.ν / d.σ^2) * exp(-(x^2 + d.ν^2) / (2*d.σ^2)) / d.σ^2
 
@@ -78,7 +90,7 @@ end
 @inline function _∇logpdf(d::Rician, x::Real)
     σ, ν = d.σ, d.ν
     σ2 = σ^2
-    B = besseli1i0m1(x*ν/σ2)
+    B = _besseli1i0m1(x*ν/σ2)
     return (σ, ν, σ2, B)
 end
 @inline function ∇logpdf(d::Rician, x::Real)
@@ -103,8 +115,8 @@ end
     σ, ν = d.σ, d.ν
     σ2 = σ^2
     z = x*ν/σ2
-    B = besseli1i0m1(z)
-    B′ = (1 + besseli2i0(z))/2 - (B + 1)^2
+    B = _besseli1i0m1(z)
+    B′ = (1 + _besseli2i0(z))/2 - (B + 1)^2
     return (x^2 * B′ - σ2) / σ2^2
 end
 
@@ -112,7 +124,7 @@ end
 @inline Distributions.rand(rng::Distributions.AbstractRNG, d::Rician{T}) where {T} = sqrt((d.ν + d.σ * randn(rng, T))^2 + (d.σ * randn(rng, T))^2)
 
 #### Testing
-#= laguerre½
+#= _laguerre½
 let
     vals = Dict{Int,BigFloat}(
         1       => big"1.44649134408317183335863878689608088636624386024796164711195852984091354675405604850815082040598215708783541465221",
@@ -124,23 +136,26 @@ let
     for (x,v) in sort(vals; by = first)
         println("\nx = $x:")
         for T in [Float64, Float32]
-            F = x == 150 ? 250 : x == 250 ? 10 : 2
-            if x <= 150
-                display(abs(_L½_bessel_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_L½_bessel_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
-            if x >= 150
-                display(abs(_L½_series_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_L½_series_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
+            display(abs(_laguerre½(T(-x)) - v) < 5 * eps(T(v)))
+            # F = x == 150 ? 250 : x == 250 ? 10 : 2
+            # display(abs(_laguerre½(T(-x)) - v))
+            # display(eps(T(v)))
+            # if x <= 150
+            #     display(abs(_L½_bessel_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_L½_bessel_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
+            # if x >= 150
+            #     display(abs(_L½_series_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_L½_series_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
         end
     end
 end;
 =#
 
-#= logbesseli0
+#= _logbesseli0
 let
     vals = Dict{Int,BigFloat}(
         1       => big"0.235914358507178648689414846199951780553937315868442236933",
@@ -152,23 +167,26 @@ let
     for (x,v) in sort(vals; by = first)
         println("\nx = $x:")
         for T in [Float64, Float32]
-            F = x == 1 ? 5 : 2
-            if x <= 75
-                display(abs(_logI0_bessel_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_logI0_bessel_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
-            if x >= 75
-                display(abs(_logI0_series_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_logI0_series_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
+            display(abs(_logbesseli0(T(x)) - v) < 5 * eps(T(v)))
+            # display(abs(_logbesseli0(T(x)) - v))
+            # display(eps(T(v)))
+            # F = x == 1 ? 5 : 2
+            # if x <= 75
+            #     display(abs(_logI0_bessel_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_logI0_bessel_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
+            # if x >= 75
+            #     display(abs(_logI0_series_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_logI0_series_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
         end
     end
 end;
 =#
 
-#= logbesseli1
+#= _logbesseli1
 let
     vals = Dict{Int,BigFloat}(
         1       => big"-0.57064798749083128142317323666480514121275701003814227921",
@@ -180,23 +198,26 @@ let
     for (x,v) in sort(vals; by = first)
         println("\nx = $x:")
         for T in [Float64, Float32]
-            F = x == 1 ? 5 : 2
-            if x <= 75
-                display(abs(_logI1_bessel_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_logI1_bessel_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
-            if x >= 75
-                display(abs(_logI1_series_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_logI1_series_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
+            display(abs(_logbesseli1(T(x)) - v) < 5 * eps(T(v)))
+            # display(abs(_logbesseli1(T(x)) - v))
+            # display(eps(T(v)))
+            # F = x == 1 ? 5 : 2
+            # if x <= 75
+            #     display(abs(_logI1_bessel_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_logI1_bessel_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
+            # if x >= 75
+            #     display(abs(_logI1_series_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_logI1_series_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
         end
     end
 end;
 =#
 
-#= besseli1i0m1
+#= _besseli1i0m1
 let
     vals = Dict{Int,BigFloat}(
         1       => big"-0.55361003410346549295231820480735733022374685259961217713",
@@ -207,25 +228,28 @@ let
         1000000 => big"-5.000001250001250001953129062510478547812614665076603e-7"
     )
     for (x,v) in sort(vals; by = first)
-        println("\nx = $x:")
         for T in [Float64, Float32]
-            F = x == 50 ? 250 : x == 75 ? 10 : 2
-            if x <= 50
-                display(abs(_I1I0m1_bessel_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_I1I0m1_bessel_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
-            if x >= 50
-                display(abs(_I1I0m1_series_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_I1I0m1_series_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
+            println("\nx = $x ($T):")
+            display(abs(_besseli1i0m1(T(x)) - v) < 25 * eps(T(v)))
+            # display(abs(_besseli1i0m1(T(x)) - v))
+            # display(eps(T(v)))
+            # F = x == 50 ? 250 : x == 75 ? 10 : 2
+            # if x <= 50
+            #     display(abs(_I1I0m1_bessel_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_I1I0m1_bessel_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
+            # if x >= 50
+            #     display(abs(_I1I0m1_series_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_I1I0m1_series_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
         end
     end
 end;
 =#
 
-#= besseli2i0
+#= _besseli2i0
 let
     vals = Dict{Int,BigFloat}(
         1       => big"0.107220068206930985904636409614714660447493705199224354277",
@@ -238,17 +262,20 @@ let
     for (x,v) in sort(vals; by = first)
         for T in [Float64, Float32]
             println("\nx = $x ($T):")
-            F = x == 1 ? 5 : 2
-            if x <= 50
-                display(abs(_I2I0_bessel_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_I2I0_bessel_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
-            if x >= 50
-                display(abs(_I2I0_series_kernel(T(x)) - v) < F * eps(T(v)))
-                # display(abs(_I2I0_series_kernel(T(x)) - v))
-                # display(eps(T(v)))
-            end
+            display(abs(_besseli2i0(T(x)) - v) < 5 * eps(T(v)))
+            # display(abs(_besseli2i0(T(x)) - v))
+            # display(eps(T(v)))
+            # F = x == 1 ? 5 : 2
+            # if x <= 50
+            #     display(abs(_I2I0_bessel_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_I2I0_bessel_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
+            # if x >= 50
+            #     display(abs(_I2I0_series_kernel(T(x)) - v) < F * eps(T(v)))
+            #     # display(abs(_I2I0_series_kernel(T(x)) - v))
+            #     # display(eps(T(v)))
+            # end
         end
     end
 end;
@@ -275,11 +302,14 @@ end
 let
     ν, σ, x = 100*rand(), 100*rand(), 100*rand()
     d = Rician(ν, σ)
-    δ = cbrt(eps())
     ∇ = ∇logpdf(d, x)
-    ∇ν = (logpdf(Rician(ν + δ, σ), x) - logpdf(Rician(ν - δ, σ), x)) / 2δ
-    ∇σ = (logpdf(Rician(ν, σ + δ), x) - logpdf(Rician(ν, σ - δ), x)) / 2δ
-    ∇δ = (ν = ∇ν, σ = ∇σ)
+    # δ = cbrt(eps())
+    # ∇ν = (logpdf(Rician(ν + δ, σ), x) - logpdf(Rician(ν - δ, σ), x)) / 2δ
+    # ∇σ = (logpdf(Rician(ν, σ + δ), x) - logpdf(Rician(ν, σ - δ), x)) / 2δ
+    ∇ν = FiniteDifferences.central_fdm(3,1)(_ν -> logpdf(Rician(_ν, σ), x), ν)
+    ∇σ = FiniteDifferences.central_fdm(3,1)(_σ -> logpdf(Rician(ν, _σ), x), σ)    
+    ∇δ = (∇ν = ∇ν, ∇σ = ∇σ)
+    display(∇); display(values(∇δ))
     display(map((x,y) -> (x-y)/y, values(∇δ), values(∇)))
 end;
 =#
@@ -290,10 +320,11 @@ let
     d = Rician(ν, σ)
     δ = eps(ν)^(1/4)
     ∇ν² = ∂²logpdf_∂ν²(d, x)
-    ∇νδ² = (logpdf(Rician(ν + δ, σ), x) - 2 * logpdf(Rician(ν, σ), x) + logpdf(Rician(ν - δ, σ), x)) / δ^2
+    # ∇νδ² = (logpdf(Rician(ν + δ, σ), x) - 2 * logpdf(Rician(ν, σ), x) + logpdf(Rician(ν - δ, σ), x)) / δ^2
+    ∇νδ² = FiniteDifferences.central_fdm(5,2)(_ν -> logpdf(Rician(_ν, σ), x), ν)
     display(∇ν²)
     display(∇νδ²)
-    display(∇ν² - ∇νδ²)
+    display((∇ν² - ∇νδ²) / max(abs(∇ν²), abs(∇νδ²)))
 end;
 =#
 
@@ -306,16 +337,9 @@ for ν in [0, 1, 10], σ in [1e-3, 1.0, 10.0]
 end
 =#
 
-####
 #### ChainRules
-####
 
-#=
-_besselix(ν::Real, x::Real) = SpecialFunctions.besselix(oftype(float(x), ν), float(x)) # besselix(nu, x) = I_nu(x) * exp(-|x|)
-_logbesseli0(x::Real) = log(_besselix(0, x)) + abs(x) # since log(besselix(nu, x)) = log(I_nu(x)) - |x|
-_logbesseli1(x::Real) = log(_besselix(1, x)) + abs(x)
-
-# Define chain rule
+# Define chain rule (Zygote > v"0.4.20" reads ChainRules directly)
 ChainRules.@scalar_rule(
     _besselix(ν::Real, x::Real),
     (ChainRules.DoesNotExist(), (_besselix(ν - 1, x) + _besselix(ν + 1, x)) / 2 - sign(x) * Ω),
@@ -327,6 +351,7 @@ function _besselix(ν::Real, x::ForwardDiff.Dual{T,V,N}) where {T,V,N}
     return ForwardDiff.Dual{T,V,N}(y, ẏ * ForwardDiff.partials(x))
 end
 
+#= Test ChainRules
 for T in [Float32, Float64], ν in [0, zero(T), 1, one(T), rand(T), -rand(T)], x in [exp(one(T)), T(pi), one(T)]
     for f in [x -> _besselix(ν, x), x -> cos(2 * _besselix(ν, x)^2), _logbesseli0]
         δ_fd  = FiniteDifferences.central_fdm(5,1)(f, x)
